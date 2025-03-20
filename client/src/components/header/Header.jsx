89ee6c11185e90fffdfd37ca6b99626dacb8a4f6
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2025-03-20 20:16:03 +0200

    Add dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function Header() {
+    const { email } = useContext(UserContext);
+
     return (
         <header>
             {/* <!-- Navigation --> */}
@@ -8,15 +12,20 @@ export default function Header() {
             <nav>
                 <Link to="/games">All games</Link>
                 {/* <!-- Logged-in users --> */}
-                <div id="user">
-                    <Link to="/games/create">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                {/* <!-- Guest users --> */}
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {email ? (
+                    <div id="user">
+                        <Link to="/games/create">Create Game</Link>
+                        <Link to="/logout">Logout</Link>
+                        {email}
+                    </div>
+                )
+                    : (
+                        <div id="guest">
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                    )
+                }
             </nav>
         </header>
     );
