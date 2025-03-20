a639548e478dbefab409dd672b60313d316b863b
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2025-03-20 13:16:09 +0200

    Use context for authData

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import GameCreate from './components/game-create/GameCreate'
 import GameDetails from './components/game-details/GameDetails'
 import GameEdit from './components/game-edit/GameEdit'
 import { useState } from 'react'
+import { UserContext } from './contexts/UserContext'
 
 function App() {
   const [authData, setAuthData] = useState({});
@@ -18,23 +19,25 @@ function App() {
   }
 
   return (
-    <div id="box">
+    <UserContext.Provider value={{ ...authData, userLoginHandler }} >
+      <div id="box">
 
-      <Header />
+        <Header />
 
-      <main id="main-content">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/games' element={<GameCatalog />} />
-          <Route path='/games/create' element={<GameCreate />} />
-          <Route path='/games/:gameId/details' element={<GameDetails email={authData.email}/>} />
-          <Route path='/games/:gameId/edit' element={<GameEdit />} />
-          <Route path='/login' element={<Login onLogin={userLoginHandler} />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
+        <main id="main-content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/games' element={<GameCatalog />} />
+            <Route path='/games/create' element={<GameCreate />} />
+            <Route path='/games/:gameId/details' element={<GameDetails />} />
+            <Route path='/games/:gameId/edit' element={<GameEdit />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
 
-      </main>
-    </div>
+        </main>
+      </div>
+    </UserContext.Provider>
   )
 }
 
